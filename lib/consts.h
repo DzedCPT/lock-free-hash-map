89111bccb53d7dc54e8f9e9b61af1e6c47b5ec15
DzedCPT <accounts@jeddaboyle.com>
2024-07-15 21:54:45 +0200

    Put consts into seperate file.

---
+++ lib/consts.h
@@ -0,0 +1,12 @@
+//
+// Created by Jed Boyle on 15/07/2024.
+//
+
+#ifndef CONSTS_H
+#define CONSTS_H
+
+float const DEFAULT_MAX_LOAD_RATIO = 0.5;
+std::size_t const COPY_CHUNK_SIZE = 8;
+
+
+#endif //CONSTS_H
