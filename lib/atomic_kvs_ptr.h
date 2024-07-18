51b19af4cd47c33a12aed1d5f0dfb13d8ce64ef5
DzedCPT <accounts@jeddaboyle.com>
2024-07-18 14:20:21 +0200

    kvs ptr swap: Add returned ptr, building but unit tests broken

--- lib/atomic_kvs_ptr.h
+++ lib/atomic_kvs_ptr.h
@@ -6,15 +6,30 @@
 #include <utility>
 #include "kvs.h"
 
+
+template <typename K, typename V>
+class AtomicKvsPtr;
+
+template<typename K, typename V>
+class Ptr {
+public:
+    explicit Ptr(AtomicKvsPtr<K, V>* kvs, KeyValueStore<K, V>*);
+    ~Ptr();
+    KeyValueStore<K, V>* ref();
+    KeyValueStore<K, V>* mKvs;
+    AtomicKvsPtr<K, V>* mAtomicPtr;
+};
+
 template <typename K, typename V>
 class AtomicKvsPtr {
 public:
-    AtomicKvsPtr(KeyValueStore<K, V>* kvs): mKvs(kvs) {}
+    explicit AtomicKvsPtr(KeyValueStore<K, V>* kvs): mKvs(kvs) {}
 
-	KeyValueStore<K, V>* load() {
-		return mKvs;
+	Ptr<K, V> load() {
+		return Ptr<K, V>(this, mKvs);
 	}
 
+        // ZZZ: Maybe a better name for this would be const load?
 	KeyValueStore<K, V>* constLoad() const {
 		return mKvs;
 	}
@@ -24,8 +39,23 @@ public:
 	}
 
     std::atomic<KeyValueStore<K, V>*> mKvs;
-    std::atomic<int> mRefCount;
+    std::atomic<int> mRefCount{};
 };
+
+template <typename K, typename V>
+Ptr<K, V>::Ptr(AtomicKvsPtr<K, V>* kvs, KeyValueStore<K, V>* kvs2) : mKvs(kvs2), mAtomicPtr(kvs) {}
+
+template <typename K, typename V>
+Ptr<K, V>::~Ptr() {
+    // --mKvs->mRefCount;
+}
+
+template <typename K, typename V>
+KeyValueStore<K, V>* Ptr<K, V>::ref() {
+    return mKvs;
+}
+
+
 
 
 #endif  // WRAPPER_H
--- lib/map.cpp
+++ lib/map.cpp
@@ -19,15 +19,15 @@ ConcurrentUnorderedMap<K, V>::ConcurrentUnorderedMap(int exp,
 template <typename K, typename V>
 V ConcurrentUnorderedMap<K, V>::insert(std::pair<K, V> const& val) {
     tryUpdateKvsHead();
-    return mHeadKvs.load()->insert(val);
+    return mHeadKvs.load().ref()->insert(val);
 }
 template <typename K, typename V>
 V ConcurrentUnorderedMap<K, V>::at(K const key) {
-    return mHeadKvs.load()->at(key);
+    return mHeadKvs.load().ref()->at(key);
 }
 template <typename K, typename V>
 size_t ConcurrentUnorderedMap<K, V>::bucket_count() {
-    return mHeadKvs.load()->bucket_count();
+    return mHeadKvs.load().ref()->bucket_count();
 }
 
 template <typename K, typename V>
@@ -37,18 +37,19 @@ size_t ConcurrentUnorderedMap<K, V>::size() const {
 
 template <typename K, typename V>
 bool ConcurrentUnorderedMap<K, V>::empty() {
-    return mHeadKvs.load()->empty();
+    return mHeadKvs.load().ref()->empty();
 }
 
 template <typename K, typename V>
 size_t ConcurrentUnorderedMap<K, V>::depth() {
     size_t depth = 0;
-    KeyValueStore<K, V>* kvs = mHeadKvs.load();
+    auto kvs = mHeadKvs.load();
     while (true) {
-        if (kvs->nextKvs() == nullptr) {
+        if (kvs.ref()->nextKvs() == nullptr) {
             break;
         }
-        kvs = kvs->nextKvs();
+		// ZZZ: NEed to fix:
+        // kvs = kvs.ref()->nextKvs();
         depth++;
     }
     return depth;
@@ -73,17 +74,17 @@ bool ConcurrentUnorderedMap<K, V>::operator==(
 
 template <typename K, typename V>
 void ConcurrentUnorderedMap<K, V>::erase(K const key) {
-    mHeadKvs.load()->erase(key);
+    mHeadKvs.load().ref()->erase(key);
 }
 
 template <typename K, typename V>
 void ConcurrentUnorderedMap<K, V>::tryUpdateKvsHead() {
     // Surgically replace the head.
     auto headKvs = mHeadKvs.load();
-    auto nextKvs = headKvs->nextKvs();
-    if (nextKvs != nullptr && headKvs->copied() &&
-        !headKvs->hasActiveReaders()) {
-        if (mHeadKvs.exchange(headKvs, nextKvs)) {
+    auto nextKvs = headKvs.ref()->nextKvs();
+    if (nextKvs != nullptr && headKvs.ref()->copied() &&
+        !headKvs.ref()->hasActiveReaders()) {
+        if (mHeadKvs.exchange(headKvs.ref(), nextKvs)) {
         // We won so it's out responsibility to clean up the old Kvs
         // TODO NB: Will need to put this back, but currently it creates
         // segfault sometimes. delete headValue;/* ; */
